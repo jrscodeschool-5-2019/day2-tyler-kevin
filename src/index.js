8e8d2d0353edf845c98b3272e871b239fa61b5b5
Anna Fulton <afulton7@gmail.com>
2019-07-01 14:10:04 -0400

    Kevin as nav

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
-import './equals_test'
-import tapBrowserColor from 'tap-browser-color'
-tapBrowserColor()
-  /*
+import './equals_test';
+import './add_test';
+
+import tapBrowserColor from 'tap-browser-color';
+tapBrowserColor();
+
+/*
 const run = async function() {
   return await import('./test')
 }
 
 run()
 */
-
